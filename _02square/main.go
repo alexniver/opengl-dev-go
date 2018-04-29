9cc67519b69b83435ef6a50af8fad538436a662e
alexniver <zhulin10541@gmail.com>
2018-04-29 19:50:51 +0800

    add another shader make two rect

--- _02square/main.go
+++ _02square/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
-var vertexShaderSource = `
+var vertexShaderSource1 = `
 #version 330
 in vec3 vp;
 void main() {
@@ -18,11 +18,28 @@ void main() {
 }
 ` + "\x00"
 
-var fragmentShaderSource = `
+// this vertexShader makes point pos's x=-x
+var vertexShaderSource2 = `
+#version 330
+in vec3 vp;
+void main() {
+	gl_Position = vec4(-vp.x, vp.y, vp.z, 1.0);
+}
+` + "\x00"
+
+var fragmentShaderSource1 = `
 #version 330
 out vec4 frag_colour;
 void main() {
 	frag_colour = vec4(0, 1, 1, 1);
+}
+` + "\x00"
+
+var fragmentShaderSource2 = `
+#version 330
+out vec4 frag_colour;
+void main() {
+	frag_colour = vec4(0, 0, 1, 1);
 }
 ` + "\x00"
 
@@ -51,17 +68,13 @@ func initGlfw() *glfw.Window {
 	return window
 }
 
-func initOpenGL() uint32 {
+func initOpenGL() {
 	// init gl and get a program
 	if err := gl.Init(); nil != err {
 		panic(err)
 	}
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("Opengl version", version)
-	prog := gl.CreateProgram()
-
-	return prog
-
 }
 
 func makeVbo(vertices []float32) uint32 {
@@ -114,29 +127,39 @@ func main() {
 	window := initGlfw()
 	defer glfw.Terminate()
 
-	prog := initOpenGL()
+	initOpenGL()
+	prog1 := gl.CreateProgram()
+	prog2 := gl.CreateProgram()
 
 	// attach shader
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	vertexShader1, err := compileShader(vertexShaderSource1, gl.VERTEX_SHADER)
 	if nil != err {
 		log.Panic(err)
 	}
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	fragmentShader1, err := compileShader(fragmentShaderSource1, gl.FRAGMENT_SHADER)
 	if nil != err {
 		log.Panic(err)
 	}
-	gl.AttachShader(prog, vertexShader)
-	gl.AttachShader(prog, fragmentShader)
-	gl.LinkProgram(prog)
-
-	gl.DeleteShader(vertexShader)
-	gl.DeleteShader(fragmentShader)
+	vertexShader2, err := compileShader(vertexShaderSource2, gl.VERTEX_SHADER)
+	if nil != err {
+		log.Panic(err)
+	}
+	fragmentShader2, err := compileShader(fragmentShaderSource2, gl.FRAGMENT_SHADER)
+	if nil != err {
+		log.Panic(err)
+	}
+	gl.AttachShader(prog1, vertexShader1)
+	gl.AttachShader(prog1, fragmentShader1)
+	gl.LinkProgram(prog1)
+	gl.AttachShader(prog2, vertexShader2)
+	gl.AttachShader(prog2, fragmentShader2)
+	gl.LinkProgram(prog2)
 
 	vertices := []float32{
+		-1, 0.5, 0,
 		-0.5, 0.5, 0,
-		0.5, 0.5, 0,
-		0.5, -0.5, 0,
 		-0.5, -0.5, 0,
+		-1, -0.5, 0,
 	}
 
 	indices := []uint32{
@@ -153,16 +176,16 @@ func main() {
 	defer gl.DeleteBuffers(1, &vao)
 	defer gl.DeleteBuffers(1, &ebo)
 
-	gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
+	// gl.PolygonMode(gl.FRONT_AND_BACK, gl.LINE)
 
 	for !window.ShouldClose() {
 		gl.ClearColor(0.5, 0.5, 1, 1.0)
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		gl.UseProgram(prog)
+		gl.UseProgram(prog1)
 
-		gl.BindVertexArray(vao)
-		// gl.DrawArrays(gl.TRIANGLES, 0, int32(len(vertices)/3))
-		// gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.Ptr(indices))
+		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.PtrOffset(0))
+
+		gl.UseProgram(prog2)
 		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.PtrOffset(0))
 
 		glfw.PollEvents()
