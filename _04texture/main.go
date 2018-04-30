301ffe48a61b81b517ced60697fdd91b5202b5d9
alexniver <zhulin10541@gmail.com>
2018-04-30 22:32:09 +0800

    texture

---
+++ _04texture/main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"runtime"
+	"strings"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const windowWidth = 800
+const windowHeight = 600
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	initOpenGL()
+
+	window.SetKeyCallback(keyCallback)
+
+	shaderProgram := gl.CreateProgram()
+
+	// read shader from files
+	verticsShaderHandle, err := readShaderFromFile("./shaders/vertices.vert", gl.VERTEX_SHADER)
+	if nil != err {
+		log.Panic(err)
+	}
+	fragmentShaderHandle, err := readShaderFromFile("./shaders/fragment.frag", gl.FRAGMENT_SHADER)
+	if nil != err {
+		log.Panic(err)
+	}
+
+	// gen program
+	gl.AttachShader(shaderProgram, verticsShaderHandle)
+	gl.AttachShader(shaderProgram, fragmentShaderHandle)
+	gl.LinkProgram(shaderProgram)
+
+	// vertices and indices
+	vertices := []float32{
+		// top         // color
+		0.0, 0.5, 0.0, 1.0, 0.0, 0.0,
+
+		0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+
+		-0.5, -0.5, 0.0, 0.0, 0.0, 1.0,
+	}
+
+	indices := []uint32{
+		0, 1, 2,
+	}
+
+	/*VBO := makeVbo(vertices)
+	makeVao(VBO)
+	// pos
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+
+	// color
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
+
+	makeEbo(indices)*/
+
+	var VAO uint32
+	gl.GenVertexArrays(1, &VAO)
+	gl.BindVertexArray(VAO)
+
+	var VBO uint32
+	gl.GenBuffers(1, &VBO)
+	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(vertices), gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	// pos
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+
+	// color
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 6*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
+
+	var EBO uint32
+	gl.GenBuffers(1, &EBO)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, 4*len(indices), gl.Ptr(indices), gl.STATIC_DRAW)
+
+	for !window.ShouldClose() {
+		gl.ClearColor(0.5, 0.5, 1, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		offset := float32(0.5)
+		offsetLoc := gl.GetUniformLocation(shaderProgram, gl.Str("offset"+"\x00"))
+
+		gl.UseProgram(shaderProgram)
+		gl.Uniform1f(offsetLoc, offset)
+		gl.DrawElements(gl.TRIANGLES, 3, gl.UNSIGNED_INT, gl.PtrOffset(0))
+
+		glfw.PollEvents()
+		window.SwapBuffers()
+	}
+
+}
+
+func initGlfw() *glfw.Window {
+	// init glfw
+	if err := glfw.Init(); nil != err {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(800, 600, "Trinagle", nil, nil)
+	if nil != err {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func initOpenGL() {
+	// init gl and get a program
+	if err := gl.Init(); nil != err {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("Opengl version", version)
+}
+
+func makeVbo(vertices []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(vertices), gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	return vbo
+}
+
+func makeVao(vbo uint32) uint32 {
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	return vao
+}
+
+func makeEbo(indices []uint32) uint32 {
+	var ebo uint32
+	gl.GenBuffers(1, &ebo)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, 4*len(indices), gl.Ptr(indices), gl.STATIC_DRAW)
+	return ebo
+}
+
+func readShaderFromFile(file string, sType uint32) (uint32, error) {
+	// read and compile
+	src, err := ioutil.ReadFile(file)
+	if nil != err {
+		return 0, err
+	}
+
+	return compileShader(string(src)+"\x00", sType)
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if gl.FALSE == status {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+		return 0, fmt.Errorf("failed to compile %v : %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func keyCallback(
+	window *glfw.Window,
+	key glfw.Key,
+	scancode int,
+	action glfw.Action,
+	mods glfw.ModifierKey,
+) {
+	if key == glfw.KeyEscape && action == glfw.Press {
+		window.SetShouldClose(true)
+	}
+}
