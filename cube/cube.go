9e34767778b8e17d49d1dfd016a694faa264fc83
alexniver <zhulin10541@gmail.com>
2018-06-22 16:14:35 +0800

    change package to main

--- cube/cube.go
+++ cube/cube.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Renders a textured spinning cube using GLFW 3 and OpenGL 4.1 core forward-compatible profile.
-package cube // import "github.com/go-gl/example/gl41core-cube"
+package main // import "github.com/go-gl/example/gl41core-cube"
 
 import (
 	"fmt"
@@ -320,7 +320,7 @@ var cubeVertices = []float32{
 
 // Set the working directory to the root of Go package, so that its assets can be accessed.
 func init() {
-	dir, err := importPathToDir("github.com/go-gl/example/gl41core-cube")
+	dir, err := importPathToDir("github.com/alexniver/opengl-dev-go/cube")
 	if err != nil {
 		log.Fatalln("Unable to find Go package in your GOPATH, it's needed to load assets:", err)
 	}
